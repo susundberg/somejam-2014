952419d4e1636f437da2c148883bf25d1d7ac51b
Pauli Sundberg <pauli.sundberg@iki.fi>
2014-03-15 20:44:50 +0200

    Update pages

--- Database.py
+++ Database.py
@@ -8,11 +8,11 @@ import webapp2
 
 
 class Feeds(ndb.Model):
-    name  = ndb.StringProperty(indexed=False)
-    desc  = ndb.StringProperty(indexed=False)
-    open  = ndb.StringProperty(indexed=False)
-    close = ndb.StringProperty(indexed=False)
-    link  = ndb.StringProperty(indexed=False)
+    name    = ndb.StringProperty(indexed=False)
+    desc    = ndb.StringProperty(indexed=False)
+    open    = ndb.StringProperty(indexed=False)
+    close   = ndb.StringProperty(indexed=False)
+    link    = ndb.StringProperty(indexed=False)
     feedurl = ndb.StringProperty(indexed=False)
     type    = ndb.StringProperty(indexed=False)
 
--- mainpage.py
+++ mainpage.py
@@ -6,8 +6,13 @@ from google.appengine.ext import ndb
 
 import jinja2
 import webapp2
-
+import urllib2
 import Database
+
+import re
+
+from google.appengine.api import urlfetch
+
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -17,17 +22,42 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     
 def get_feeds():
       feeds_query = Database.Feeds.query()
-      feeds = feeds_query.fetch(99)
-      template_values = { "feeds" : feeds }
-      return template_values
+      return feeds_query.fetch(99)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        template_values = get_feeds()
+        template_values = { 'feeds' : get_feeds() }
         template = JINJA_ENVIRONMENT.get_template('templates/main.html')
         self.response.write(template.render(template_values)) 
-
         
+class UpdatePage(webapp2.RequestHandler):
+    def get(self):
+      feeds = get_feeds() 
+      for feed in feeds:
+         if feed.feedurl:
+            print "FETCH: " + feed.feedurl 
+            result = urlfetch.fetch( url = feed.feedurl + "?hl=en&max-results=4&orderby=starttime")
+            print result.headers
+            self.update_feed( feed, result )
+            
+      self.response.write("OK")   
+    def update_feed( self, feed, result ):
+       print "RESULT STATUS: " + str(result.status_code)
+       #print "RESULT CONTENT: " + str(result.content)
+       from BeautifulSoup import BeautifulStoneSoup
+       soup = BeautifulStoneSoup( result.content )
+       pattern = re.compile(".*When: (.*)&amp.*")
+       for item in soup.findAll("summary"):
+          text = item.text
+          match = pattern.match( text )
+          if match:
+            print "GOT: " + match.group(1)
+          else:
+            print "No match"
+          
+       
+    
+    
 class AdminPage(webapp2.RequestHandler):
     def get(self):
     
@@ -38,16 +68,18 @@ class AdminPage(webapp2.RequestHandler):
         feed.name = self.request.get('name')
         feed.desc = self.request.get('desc')
         feed.link = self.request.get('url')
-        feed.feed_url = self.request.get('feed_url')
+        feed.feedurl  = self.request.get('feedurl')
         feed.type     = self.request.get('type')
         feed.put()
         self.response.write("OK")
+
+        
         
         
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/admin', AdminPage),
-    ('/update', Updatepage),
+    ('/update', UpdatePage),
     ], debug=True)
 
 
